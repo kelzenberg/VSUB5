ad9e9bf23e0428b91354efa194e333cb983f85b9
omniskop <omniskopus@gmail.com>
2017-12-01 16:40:49 +0100

    Project structure improved

---
+++ src/com/client/Client.java
@@ -0,0 +1,8 @@
+package com.server;
+
+public class Client {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/com/server/Server.java
+++ src/com/server/Server.java
@@ -1,8 +1,8 @@
 package com.server;
 
-public class Main {
+public class Server {
 
     public static void main(String[] args) {
-	// write your code here
+        
     }
 }
