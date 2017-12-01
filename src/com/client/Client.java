aa40d448e5b125190f081b607799000ff8219c07
omniskop <omniskopus@gmail.com>
2017-12-01 18:28:54 +0100

    Added policy

--- src/com/client/Client.java
+++ src/com/client/Client.java
@@ -1,8 +1,27 @@
 package com.client;
 
-public class Client {
+import com.BulletinBoardIntf;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
-    public static void main(String[] args) {
-        
-    }
+public class Client {
+    
+    public static void main(String args[]) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "BulletinBoard";
+            Registry registry = LocateRegistry.getRegistry();
+            //Compute comp = (Compute) registry.lookup(name);
+            BulletinBoardIntf bb = (BulletinBoardIntf) registry.lookup(name);
+            //Pi task = new Pi(Integer.parseInt(args[1]));
+            //BigDecimal pi = comp.executeTask(task);
+            int test = bb.getMessageCount();
+            System.out.println(test);
+        } catch (Exception e) {
+            System.err.println("ComputePi exception:");
+            e.printStackTrace();
+        }
+    } 
 }
