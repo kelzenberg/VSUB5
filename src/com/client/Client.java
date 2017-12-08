a7946b9b96bce40ace9dcd472ef4a8bb913fb0f1
omniskop <omniskopus@gmail.com>
2017-12-08 18:45:04 +0100

    First version of client

--- src/com/client/Client.java
+++ src/com/client/Client.java
@@ -3,20 +3,29 @@ package com.client;
 import com.BulletinBoardIntf;
 
 import java.rmi.RemoteException;
+import com.Exceptions.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Arrays;
+import java.io.*;
+import java.lang.String;
 
 public class Client {
+
+    static BulletinBoardIntf bb;
 
     public static void main(String args[]) {
         try {
             String name = "BulletinBoard";
             Registry registry = LocateRegistry.getRegistry();
-            //Compute comp = (Compute) registry.lookup(name);
-            BulletinBoardIntf bb = (BulletinBoardIntf) registry.lookup(name);
-            //Pi task = new Pi(Integer.parseInt(args[1]));
-            //BigDecimal pi = comp.executeTask(task);
+            bb = (BulletinBoardIntf) registry.lookup(name);
+            
+            
+            
+            runCLI();
+            
+            
+            /*
             int test = bb.getMessageCount();
             System.out.println("All Messages: " + test);
             try {
@@ -27,6 +36,7 @@ public class Client {
             test = bb.getMessageCount();
             System.out.println("All Messages: " + test);
             printAllMessages(bb);
+            */
         } catch (Exception e) {
             System.err.println("ComputePi exception:");
             e.printStackTrace();
@@ -45,5 +55,87 @@ public class Client {
             System.out.println(message.toString());
         }
         System.out.println("Messages end   ===");
+    }
+    
+    private static void runCLI() {
+        BufferedReader clReader = new BufferedReader(new InputStreamReader(System.in));
+        String rawInput;
+        String[] userInput;
+        while (true) {
+            System.out.print("\n> ");
+            rawInput = readline(clReader);
+            
+            userInput = validateUserInput(rawInput);
+            if(userInput == null) {
+                continue;
+            }
+            
+            parseUserInput(userInput);
+        }
+    }
+    
+    private static String[] validateUserInput(String rawInput) {
+        rawInput = rawInput.trim();
+        if(rawInput.length() == 0) {
+            return null;
+        }
+        String[] out = rawInput.split(" ");
+        return out;
+    }
+    
+    private static void parseUserInput(String[] userInput) {
+        switch(userInput[0]) {
+            case "help": // prints help information
+                // TODO: print help information
+                // System.out.println("");
+            break;
+            case "post": // posts a message
+                if(userInput.length > 1) { // The user wrote the message directly after the post command
+                    String[] msg = Arrays.copyOfRange(userInput, 1, userInput.length);
+                    buildAndPostMessage(msg);
+                } else { // Enter post mode where the user can enter the message.
+                    
+                }
+            break;
+            case "count": // returns the number of messages on the BulletinBoard
+                
+            break;
+            case "read": // returns all messages on the BulletinBoard
+            
+            break;
+            default:
+                System.out.println("Unknown command. Type 'help' for a list of  all commands.");
+        }
+    }
+    
+    private static void buildAndPostMessage(String[] wordList) {
+        String message = String.join(" ", wordList);
+        try {
+            bb.putMessage(message);
+        } catch(InvalidMessageException e) {
+            System.out.println("The server responded with an InvalidMessageException:");
+            System.out.println(e);
+        } catch(BulletinBoardFullException e) {
+            System.out.println("The server responded with an BulletinBoardFullException:");
+            System.out.println(e);
+        } catch(RemoteException e) {
+            System.out.println("The server encountered an unknown error:");
+            System.out.println(e);
+        }
+    }
+    
+    private static String readline(BufferedReader reader) {
+        try {
+            String out = reader.readLine();
+            if(out == null) { // end of stream
+                System.out.println("End of readline stream reached. Exiting.");
+                System.exit(0);
+            }
+        } catch(IOException e) {
+            System.out.println("An error occoured while reading console input. Extiting.");
+            System.out.println(e);
+            System.exit(1);
+        }
+        return null;
     }
 }
