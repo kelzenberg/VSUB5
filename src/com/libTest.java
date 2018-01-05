c12b41e1d408925ffb36ea9a9fa1e37c17a48fd6
Steffen <kelzenberg@users.noreply.github.com>
2018-01-05 19:17:07 +0100

    basic commands added for later

--- src/com/libTest.java
+++ src/com/libTest.java
@@ -1,18 +1,30 @@
 package com;
 
+import org.apache.jena.query.Query;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.rdfconnection.RDFConnectionLocal;
 import org.apache.jena.vocabulary.VCARD;
 
 public class libTest {
 
     private final Model model;
     private final Resource johnSmith;
+    private final RDFConnection rdf;
 
     public libTest(Model model, Resource johnSmith) {
         this.model = model;
         this.johnSmith = johnSmith;
+        rdf = RDFConnectionFactory.connect("http://141.64.175.23:9999/blazegraph/sparql");
+
+        Query query = BEFEHL;
+
+        rdf.queryResultSet();
+        rdf.update();
+
     }
 
     public void test() {
