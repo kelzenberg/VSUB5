7909bad34820002920e24a25c2dc00f6033d8e6d
1r00t <root.1drone@gmail.com>
2018-01-16 21:04:52 +0100

    added statement

--- src/com/Statements.java
+++ src/com/Statements.java
@@ -14,4 +14,15 @@ public class Statements {
     public static final String insertData = "INSERT DATA { s p o . }"; //not functional, just the pattern
     public static final String insertMultiLineData = "INSERT DATA { s p o ; " + " p o . }";  //not functional, just the pattern
 
+    public static final String abfrageMitFilter = "prefix foaf: <http://xmlns.com/foaf/0.1/>\n" +
+            "prefix dc: <http://purl.org/dc/elements/1.1/>\n" +
+            "prefix omnis: <http://omniskop.de/vs/>\n" +
+            "SELECT ?user ?name ?mbox\n" +
+            "WHERE {\n" +
+            "  ?user foaf:type foaf:person .\n" +
+            "  ?user foaf:mbox ?mbox .\n" +
+            "  ?user foaf:name ?name .\n" +
+            "  FILTER(?mbox IN (\"jannis@rieger-kaulsdorf.de\", \"all@all.de\"))\n" +
+            "}";
+
 }
