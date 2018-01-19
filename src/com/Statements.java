7322a21259438621f9da3d0443a2ed075312fa14
1r00t <root.1drone@gmail.com>
2018-01-19 18:31:37 +0100

    ja

--- src/com/Statements.java
+++ src/com/Statements.java
@@ -7,22 +7,20 @@ public class Statements {
 
     // space character at the end is needed to stack commands
     public static final String prefixFOAF = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n";
-    public static final String prefixDC = "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n";
     public static final String prefixBB = "PREFIX bb: <http://omniskop.de/vs/bb>\n";
-    public static final String prefixAll = prefixBB + prefixFOAF + prefixDC;
+    public static final String prefixDATA = "PREFIX data: <https://omniskop.de/blazegraph/data/>\n";
+    public static final String prefixAll = prefixBB + prefixFOAF + prefixDATA;
 
     public static final String insertData = "INSERT DATA { s p o . }"; //not functional, just the pattern
     public static final String insertMultiLineData = "INSERT DATA { s p o ; " + " p o . }";  //not functional, just the pattern
 
-    public static final String abfrageMitFilter = "prefix foaf: <http://xmlns.com/foaf/0.1/>\n" +
-            "prefix dc: <http://purl.org/dc/elements/1.1/>\n" +
-            "prefix bb: <http://omniskop.de/vs/bb>\n" +
-            "SELECT ?user ?name ?mbox\n" +
-            "WHERE {\n" +
-            "  ?user foaf:type foaf:person .\n" +
-            "  ?user foaf:mbox ?mbox .\n" +
-            "  ?user foaf:name ?name .\n" +
-            "  FILTER(?mbox IN (\"jannis@rieger-kaulsdorf.de\", \"all@all.de\"))\n" +
-            "}";
+    public static final String createMessage = String.format("INSERT {\n" +
+            "    data:message rdf:type bb:Message ;\n" +
+            "        bb:subject \"%s\" ;\n" +
+            "        bb:content \"Eine Nachricht, die ist Lustig, eine Nachricht, die ist schön.\" ;\n" +
+            "        bb:creator data:user1 ;\n" +
+            "        bb:timestamp ?time;\n" +
+            "        bb:recipient data:user1 .\n" +
+            "} WHERE {BIND(NOW() as ?time)}");
 
 }
